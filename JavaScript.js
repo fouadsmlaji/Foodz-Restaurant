f99210095a218fef8e4ee2838763fa477895cc31
foodz66 <138889360+foodz66@users.noreply.github.com>
2024-05-21 18:10:26 +0300

    sdf

--- JavaScript.js
+++ JavaScript.js
@@ -70,3 +70,23 @@ document.getElementById("nav-toggle").onclick = function () {
       console.error("Scroller element not found!");
     }
   }
+
+
+  const config = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    gap: 32,
+    breakpoints: {
+      1280: {
+        perView: 3,
+      },
+      1024: {
+        perView: 2,
+      },
+      768: {
+        perView: 1,
+      }
+    }
+  }
+  new Glide('.glide', config).mount()
