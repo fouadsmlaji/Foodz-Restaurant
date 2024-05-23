9c8bbe2278d3602aeb7df2c50a4971ae9a340d09
foodz66 <138889360+foodz66@users.noreply.github.com>
2024-05-23 11:17:08 +0300

    done

--- JavaScript.js
+++ JavaScript.js
@@ -8,7 +8,39 @@ document.getElementById("nav-toggle").onclick = function () {
   }
 
 
+  let defaultTransform = 0;
+  function goNext() {
+      defaultTransform = defaultTransform - 320;
+      var slider = document.getElementById("slider");
+      if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+      slider.style.transform = "translateX(" + defaultTransform + "px)";
+  }
+  next.addEventListener("click", goNext);
+  function goPrev() {
+      var slider = document.getElementById("slider");
+      if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+      else defaultTransform = defaultTransform + 320;
+      slider.style.transform = "translateX(" + defaultTransform + "px)";
+  }
+  prev.addEventListener("click", goPrev)
 
+
+  let defaultTransform2 = 0;
+  function goNext2() {
+      defaultTransform2 = defaultTransform2 - 320;
+      var slider = document.getElementById("slider2");
+      if (Math.abs(defaultTransform2) >= slider.scrollWidth / 1.7) defaultTransform2 = 0;
+      slider.style.transform = "translateX(" + defaultTransform2 + "px)";
+  }
+  next2.addEventListener("click", goNext2);
+
+  function goPrev2() {
+      var slider = document.getElementById("slider2");
+      if (Math.abs(defaultTransform2) === 0) defaultTransform2 = 0;
+      else defaultTransform2 = defaultTransform2 + 320;
+      slider.style.transform = "translateX(" + defaultTransform2 + "px)";
+  }
+  prev2.addEventListener("click", goPrev2)
 
   /*
   function rotateDishPhotosOnScroll() {
